15397d31ad35071596590e960ecb718bf7941e8d
Peizhen Liao <peizhenliao23@gmail.com>
2024-04-13 16:21:13 -0400

    restructure, finish the POST part

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # SpringbootMedicalPlanApp
+## what is Oauth2.0
+![image](./assets/oauth2.0.png)
+
+## Connect to Redis server
+1. Check Redis Installation: In the Terminal window, type the following command and press Enter:
+```cd /opt/homebrew/bin/```(this is where your server installed)
+2. type in ```redis-server```
+
+After starting the server, kick off the CLI:
+1. ```redis-cli```
+2. query for all the data: ```KEYS *```
--- src/main/java/com/info7255/medicalplanapi/service/PlanService.java
+++ src/main/java/com/info7255/medicalplanapi/service/PlanService.java
@@ -1,12 +1,16 @@
 package com.info7255.medicalplanapi.service;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import redis.clients.jedis.Jedis;
+
+import javax.print.attribute.standard.JobKOctets;
 
 @Service
 public class PlanService {
@@ -31,10 +35,51 @@ public class PlanService {
 
         for(String key : jsonObject.keySet()){
             String redisKey = jsonObject.get("objectType") + ":" + jsonObject.get("objectId");
-            Object value = jsonObject.get(key);
-            jedis.hset(redisKey, key, value.toString());
-            contentMap.put(key, value);
-            map.put(redisKey, contentMap);
+            Object value = jsonObject.get(key);//traverse all the elements in this jsonObject
+
+            if(value instanceof JSONObject){//it is planCostShares
+                Map<String, Map<String, Object>> subMap = jsonToMap((JSONObject) value);//it will return a map
+                //subMap structure:
+                // key: "objectId"+"objectType"
+                // value:
+                //    "deductible": 2000,
+                //    "_org": "peggy.com",
+                //    "copay": 23,
+                //    "objectId": "1234vxc2324sdf-501",
+                //    "objectType": "membercostshare"
+                for(String subKey : subMap.keySet()){// actually it just one key in subMap
+                    Map<String, Object> subContentMap = subMap.get(subKey);
+                    subKey = subContentMap.get("objectType")+":"+subContentMap.get("objectId");
+                    jedis.set(redisKey+":"+key, subKey);//be aware of sadd, hset and set
+                }// this part store links: (look like)
+                    // key:
+                    // objectType: objectId: planCostShares:
+                    // value:
+                    // deductible,
+                    // key:
+                    // objectType: objectId: planCostShares:
+                    // value:
+                    // _org
+            }else if(value instanceof JSONArray){//it is linkedPlanServices
+                ArrayList<Object> result = jsonToList((JSONArray)value);
+
+                Map<String, Map<String, Object>> resultMap = new HashMap<>();
+
+                // Iterate through the ArrayList and convert each object to Map<String, Map<String, Object>>
+                for (Object obj : result) {
+                    // Assuming 'obj' is a Map<String, Map<String, Object>>
+                    Map<String, Map<String, Object>> subMap = (Map<String, Map<String, Object>>) obj;
+                    for(String subKey : subMap.keySet()){// actually it just one key in subMap
+                        Map<String, Object> subContentMap = subMap.get(subKey);
+                        subKey = subContentMap.get("objectType")+":"+subContentMap.get("objectId");
+                        jedis.sadd(redisKey+":"+key, subKey);//be aware of sadd, hset and set
+                    }
+                }
+            }else{//they are "_org", "objectId", "objectType" etc
+                jedis.hset(redisKey, key, value.toString());
+                contentMap.put(key, value);
+                map.put(redisKey, contentMap);
+            }
         }
         return map;
     }
@@ -84,5 +129,16 @@ public class PlanService {
             return false;
         }
         return true;
+    }
+
+    public ArrayList<Object> jsonToList(JSONArray jsonArray){//recursion until all the returns are JSON object type
+        ArrayList<Object> result = new ArrayList<>();
+        for(Object value:jsonArray){
+            if(value instanceof JSONObject){
+                result.add(jsonToMap((JSONObject) value));
+            }else if(value instanceof JSONArray){
+                result.add(jsonToList((JSONArray) value));
+            }
+        }return result;
     }
 }
