b379f148804158276a481f1803f2566af9cb43d0
Peizhen Liao <peizhenliao23@gmail.com>
2024-07-23 13:29:37 -0400

    update

--- src/main/java/com/info7255/medicalplanapi/service/KafkaConsumer_ESService.java
+++ src/main/java/com/info7255/medicalplanapi/service/KafkaConsumer_ESService.java
@@ -2,14 +2,13 @@ package com.info7255.medicalplanapi.service;
 
 import com.info7255.medicalplanapi.model.ErrorResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.GetIndexRequest;
 import com.info7255.medicalplanapi.errorHandler.*;
 import org.elasticsearch.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import org.json.JSONObject;
@@ -28,11 +27,12 @@ public class KafkaConsumer_ESService {
         this.restHighLevelClient = restHighLevelClient;
     }
 
-    public void postDocument(JSONObject plan) throws IOException {
-        if (!indexExists()) {
-            IndexRequest request = new IndexRequest(INDEX_NAME, "_doc");
+    public void postDocument(JSONObject plan, String id) throws IOException {
+        if (!idExists(INDEX_NAME, id)) {
+            IndexRequest request = new IndexRequest(INDEX_NAME, "_doc", id);
             request.source(plan, XContentType.JSON);
             try {
+                System.out.println("Hello again!");
                 restHighLevelClient.index(request, RequestOptions.DEFAULT);
             } catch (IOException e) {
                 System.out.println("postDocument error:"+e.getMessage());
@@ -42,13 +42,13 @@ public class KafkaConsumer_ESService {
         }
     }
 
-    public boolean indexExists() throws IOException {
-        GetIndexRequest request = new GetIndexRequest(INDEX_NAME);
-        return restHighLevelClient.indices().exists(request, RequestOptions.DEFAULT);
+    public boolean idExists(String INDEX_NAME, String id) throws IOException {
+        GetRequest getRequest = new GetRequest(INDEX_NAME, id);
+        return restHighLevelClient.exists(getRequest, RequestOptions.DEFAULT);
     }
 
-    public void deleteDocument(JSONObject plan) throws IOException {
-        if (!indexExists()) {
+    public void deleteDocument(JSONObject plan, String id) throws IOException {
+        if (!idExists(INDEX_NAME, id)) {
             try {
                 DeleteRequest deleteRequest = new DeleteRequest(INDEX_NAME, "_doc");
                 restHighLevelClient.delete(deleteRequest, RequestOptions.DEFAULT);
